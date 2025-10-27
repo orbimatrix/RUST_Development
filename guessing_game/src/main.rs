29adb7980187965ce5bf01eb135d5cf7f1508c17
orbimatrix <statducks99@gmail.com>
2025-10-28 01:42:03 +0500

    adding the loops in a guessing Game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,11 +1,31 @@
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
-fn main(){
+fn main() {
     println!("Guess the number!");
-    println!("Please input your guess.");
 
-    let mut guess= String::new();
+    let secret_number = rand::thread_rng().gen_range(50..=100);
+    println!("The secret number is: {}", secret_number);
 
-    io::stdin().read_line(&mut guess).expect("Failed to read line");
-    println!("You guessed: {}", guess);
+    loop {
+        println!("Please input your guess.");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = guess.trim().parse().expect("please type a number!");
+
+        println!("You guessed: {}", guess);
+
+        // comparing the guess now
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => println!("You win!"),
+        }
+    }
 }
