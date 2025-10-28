10ca156aadaf2eb969441ee3fff4b7b3d1829c0a
orbimatrix <statducks99@gmail.com>
2025-10-28 23:19:39 +0500

    controls

--- pro_concepts/src/main.rs
+++ pro_concepts/src/main.rs
@@ -1,5 +1,35 @@
+use std::io;
+
 fn main() {
 
+// dummy_function();
+let x=5;
+print_value(x);
+add_(x);
+let result=rectangle_area(10,20);
+println!("The area of the rectangle is: {}", result);
+let five=fiver();
+println!("The value returned by fiver is: {}", five);
+
+}
+
+fn add_(x:i32) {
+    println!("The value of x is: {}", x+10);
+}
+
+fn fiver()->i32{
+    5
+}
+
+fn rectangle_area(width:i32,height:i32)->i32{
+    width*height
+}
+
+fn print_value(x:i32){
+    println!("The value of x is: {}", x);
+}
+
+fn dummy_function(){
     let mut x=5;
     println!("Hello, world! The value of x is: {}", x);
     x=90;
@@ -17,7 +47,46 @@ fn main() {
     }
     println!("The value of alpha is: {}", alpha);
 
-    let mut spaces= "  ";
-    spaces=spaces.len();
-    println!("The number of spaces is: {}", spaces);
+    // let mut spaces= "  ";
+    // spaces=spaces.len();
+    // println!("The number of spaces is: {}", spaces);
+
+    let guess:u32="42".parse().expect("Not a number!");
+    println!("The value of guess is: {}", guess);
+
+    let floating_point:f64 =4.5;
+
+    let t=true;
+    let f:bool =false;
+
+    let c='z';
+    let charac: char='e';
+    println!("The characters are: {} and {}", c, charac);
+
+    let tup:(i32,f64,u8)=(1200,6.4,12);
+    let (x,y,z)=tup;
+
+    println!("The value of y is: {}", y);
+
+    let five_hundred= tup.0;
+    println!("The value of five_hundred is: {}", five_hundred);
+
+    let a:[i32;5]=[1,2,3,4,5];
+    let first=a[0];
+    let second=a[1];
+    println!("The first element is: {}, and the second element is: {}", first, second);
+
+    let array_data=[3;10];
+    println!("The array data is: {:?}", array_data);
+
+    let mut index=String::new();
+
+    io::stdin().read_line(&mut index).expect("Failed to read line");    
+    let index:usize=index.trim().parse().expect("Index not a number");  
+
+    let element=array_data[index];
+    println!("The element at index {} is: {}", index, element);
+
+
+
 }
