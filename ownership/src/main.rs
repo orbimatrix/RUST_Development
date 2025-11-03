a94b740cfee1973cb62a5f1d03ed55b97d427ab5
orbimatrix <statducks99@gmail.com>
2025-11-04 00:01:47 +0500

    ownership and shadow copy rules

---
+++ ownership/src/main.rs
@@ -0,0 +1,20 @@
+fn main() {
+    let mut s=String::from("Hello");
+    s.push_str(", world!");
+    println!("{}",s);
+
+       
+
+
+
+    let s1 = String::from("hello");
+    let s2 = s1.clone();
+
+    println!("s1 = {s1}, s2 = {s2}");
+
+
+    
+
+
+
+}
