9de05e87d321da68e285c2c666285fde3681ddd0
orbimatrix <statducks99@gmail.com>
2025-11-04 00:54:24 +0500

    function with ownership

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,20 +1,39 @@
 fn main() {
-    let mut s=String::from("Hello");
+    let mut s = String::from("Hello");
     s.push_str(", world!");
-    println!("{}",s);
-
-       
-
-
+    println!("{}", s);
 
     let s1 = String::from("hello");
     let s2 = s1.clone();
 
     println!("s1 = {s1}, s2 = {s2}");
 
+    let ss = String::from("hello");  // s comes into scope
 
-    
+    takes_ownership(ss);             // ss's value moves into the function...
+                                    // ... and so is no longer valid here
 
+    let x = 5;                      // x comes into scope
 
+    makes_copy(x);  
 
+    let len = calculate_length(&s1);
+
+    println!("The length of '{s1}' is {len}.");
+
+}
+
+fn takes_ownership(some_string: String) {
+    // some_string comes into scope
+    println!("{some_string}");
+} // Here, some_string goes out of scope and `drop` is called. The backing
+  // memory is freed.
+
+fn makes_copy(some_integer: i32) {
+    // some_integer comes into scope
+    println!("{some_integer}");
+}
+
+fn calculate_length(s: &String) -> usize {
+    s.len()
 }
